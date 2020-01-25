0338b3da06a9db700c5f3d34c45c79e073eec426
João Gabriel de Souza <joaogbdesouza@gmail.com>
2020-01-25 14:24:37 -0300

    Small changes to presentation

--- web/src/components/pokeCard/pokemon.js
+++ web/src/components/pokeCard/pokemon.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 const Pokemon = () => {
     return (
-        <div className="col-3 mt-4">
-            <div className="card" style={{width: 250}}>
-                <img className="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">Nº 001</h5>
-                    <h4>Bulbasaur</h4>
-                    <button className="btn btn-success grass">Grass</button>
-                    <button className="btn btn-success poison ml-2">Poison</button>
+        <div className="pokecardContainer col-3 mt-4">
+            <a href="#">
+                <div className="pokecard card" style={{width: 250}}>
+                    <img className="card-img-top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="pokemon" />
+                    <div className="card-body text-center">
+                        <h5 className="card-title">Nº 001</h5>
+                        <h4>Bulbasaur</h4>
+                        <button className="btn btn-success grass">Grass</button>
+                        <button className="btn btn-success poison ml-2">Poison</button>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     )
 }
