3af4de72ecfa0f388dff81878b0e229436239882
João Gabriel de Souza <joaogbdesouza@gmail.com>
2020-01-25 23:48:58 -0300

    Sorting by number

--- backend/src/controllers/PokemonController.js
+++ backend/src/controllers/PokemonController.js
@@ -3,6 +3,18 @@ const Pokemon = require('../models/Pokemon')
 module.exports = {
     async index(req, res){
         const pokemons = await Pokemon.find()
+        pokemons.sort( (pokeA, pokeB) => {
+            let comparison = 0
+            if(pokeA.number > pokeB.number) {
+                comparison = 1
+            } else if(pokeA.number < pokeB.number){
+                comparison = -1  
+            }
+
+            return comparison
+        })
+
+        console.log(pokemons)
         return res.json({
             pokemons
         })
