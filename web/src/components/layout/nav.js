22acfcc84268a561accf8bda3ebc2572b168a04e
João Gabriel de Souza <joaogbdesouza@gmail.com>
2020-02-02 19:03:20 -0300

    Fixing mobile issues

--- web/src/components/layout/nav.js
+++ web/src/components/layout/nav.js
@@ -3,20 +3,22 @@ import './nav.css'
 
 const Nav = ({setSearch, loadPokemons}) => {
     return (
-        <div className="navigation">
-            <div className="row">
-                <div className="col-5 mt-3 text-right">
-                    <label className="searchInput" htmlFor="">Search for your pokemon</label>
+        <nav>
+            <div className="nav-wrapper navigation">
+                <div className="row">
+                    <div className="col-5 mt-3 text-right">
+                        <label className="searchInput" htmlFor="">Search for your pokemon</label>
 
-                </div>
-                <div className="col-3 mt-4 text-left">
-                    <input onChange={(e) => setSearch(e.currentTarget.value.toLowerCase())} type="text" className="form-control form-control-lg" />
-                </div>
-                <div className="col-1 mt-4  text-left">
-                    <button onClick={loadPokemons} className="btn btn-lg btn-success">Oi</button>
+                    </div>
+                    <div className="col-3 mt-4 text-left">
+                        <input onChange={(e) => setSearch(e.currentTarget.value.toLowerCase())} type="text" className="form-control form-control-lg" />
+                    </div>
+                    <div className="col-1 mt-4  text-left">
+                        <button onClick={loadPokemons} className="btn btn-lg btn-success">Oi</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </nav>
     )
 }
 
